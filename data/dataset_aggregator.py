047b8a0b937b48fae25929dfc20c04f272f26ee1
Adrian Amir <aami0016@student.monash.edu>
2022-10-16 16:30:29 +0800

    DATA PUSH

--- data/dataset_aggregator.py
+++ data/dataset_aggregator.py
@@ -9,8 +9,11 @@ country_count = data['Country'].value_counts()
 country_count.columns = ['Count']
 country_count.to_csv('data\Billionaire_Count_by_Country.csv', sep = ',', encoding ='utf-8')
 """
-"""
+
 # country_totall_mean_networth
+country_count = data['Country'].value_counts()
+country_count.info()
+
 result_mean = data.groupby('Country')['Net Worth'].mean().sort_values(ascending=False)
 print(result_mean)
 
@@ -18,11 +21,14 @@ result_sum = data.groupby('Country')['Net Worth'].sum().sort_values(ascending=Fa
 print(result_sum)
 
 merged_inner = pd.merge(left=result_sum, right=result_mean, left_on='Country', right_on='Country')
-merged_inner.columns = ['Total', 'Mean']
+merged_inner = merged_inner.merge(country_count.to_frame(), left_index=True,right_index=True)
+merged_inner.columns = ['Total', 'Mean', 'Count']
 print(merged_inner)
+pd.to_numeric(merged_inner['Count'])
+merged_inner['Count'].info()
 merged_inner.to_csv('data\Country_Average_Total.csv', sep = ',', encoding ='utf-8')
-"""
-"""
+
+""""
 # Industry_Total_Mean
 result_mean = data.groupby('Industry')['Net Worth'].mean().sort_values(ascending=False)
 print(result_mean)
@@ -34,5 +40,5 @@ merged_inner = pd.merge(left=result_sum, right=result_mean, left_on='Industry', 
 merged_inner.columns = ['Total', 'Mean']
 print(merged_inner)
 merged_inner.to_csv('data\Industry_Average_Total.csv', sep = ',', encoding ='utf-8')
-"""
 
+"""
